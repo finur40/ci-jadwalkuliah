aff5cf48e03a846dec437b200d87e6f63871515b
finur40 <fitranour40@gmail.com>
2025-04-14 16:10:03 +0800

    Update ScheduleModel.php

--- app/Models/ScheduleModel.php
+++ app/Models/ScheduleModel.php
@@ -5,5 +5,5 @@ use CodeIgniter\Model;
 class ScheduleModel extends Model
 {
     protected $table = 'schedules';
-    protected $allowedFields = ['course_id', 'instructor_id', 'classroom_id', 'day', 'start_time', 'end_time'];
+    protected $allowedFields = ['course_id', 'classroom_id', 'day', 'start_time', 'end_time'];
 }
